f96c6e44e6496b839bf71565d9f13d3b4bda117d
Sicon <jonathan3492@hotmail.com>
2021-05-11 20:37:50 -0600

    ConexionBDD

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
 
-$link = 'mysql:host=localhost; dbname=pizzanswer';
-$usuario = 'root';
-$pass = 'jioortiz';
+$link = 'mysql:host=sql3.freesqldatabase.com:3306/; dbname=sql3411883';
+$usuario = 'sql3411883';
+$pass = 'I8r8vJ2nEp';
 
 try {
     $pdo = new PDO($link, $usuario, $pass);
