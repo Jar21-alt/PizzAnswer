3c3611ef2aafda2facdcb2210c76b138a9e7f512
SergioAlejandroGuzmanAlvarez <72154333+SergioAlejandroGuzmanAlvarez@users.noreply.github.com>
2021-05-11 22:14:53 -0600

    cambioestilo

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 include_once 'conexion.php';
 ?>
+<!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
@@ -8,7 +9,7 @@ include_once 'conexion.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='stylesheet' type="text/css" href="estilo.css">
     <link rel="stylesheet" type="text/css" href="fonts.css">
-    <title>PrincipalCliente</title>
+    <title>Inicio</title>
 </head>
 <body>
     <header class="encabezadomenu">
