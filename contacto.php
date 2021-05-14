d7a62cb77045668ea3745b5884e75b5d606a4417
Sicon <jonathan3492@hotmail.com>
2021-05-13 23:44:05 -0600

    ContactoAvance2

--- contacto.php
+++ contacto.php
@@ -1,18 +1,32 @@
 <?php
 include_once 'conexion.php';
+
+$sql_leer = 'SELECT * FROM datospizzeria';
+
+$gsent = $pdo->prepare($sql_leer);
+$gsent->execute();
+
+$resultado = $gsent->fetchAll();
+
+//var_dump($resultado);
+
 ?>
+<!doctype html>
 <html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link rel='stylesheet' type="text/css" href="estilo.css">
     <link rel='stylesheet' type="text/css" href="contacto.css">
     <title>Contacto</title>
 </head>
 
 <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
     <header>
         <nav class="menuPizza">
             <img src="images/logo.PNG" class="logoP">
@@ -37,29 +51,22 @@ include_once 'conexion.php';
                 </div>
                 <div class='column'>
                     <div class='right-column'>
-                        <div class="datosPizza">
-                            <div class="nombrePiz">Pizzeria Centro</div>
-                            <div class="direcPiz">Puebla 73</div>
-                            <div class="telPiz">311 225 6581</div>
-                            <a href="https://www.facebook.com/DominosMexico/" target="_blank"><img src="images/logoFacebook.png" alt=""></a>
-                            <a href="https://g.page/DominosManglar?share" target="_blank"><img src="images/logoMaps.png" alt=""></a>
-                        </div>
-                        <div class="datosPizza">
-                            <div class="nombrePiz">Pizzeria Villas</div>
-                            <div class="direcPiz">Lomas Agrias 12</div>
-                            <div class="telPiz">311 297 6545</div>
-                            <a href="https://www.facebook.com/DominosMexico/" target="_blank"><img src="images/logoFacebook.png" alt=""></a>
-                            <a href="https://g.page/DominosManglar?share" target="_blank"><img src="images/logoMaps.png" alt=""></a>
-                        </div>
+                        <?php foreach ($resultado as $dato) : ?>
+                            <div class="datosPizza">
+                                <div class="nombrePiz"><?php echo $dato['nombrePizzeria'] ?></div>
+                                <div class="direcPiz"><?php echo $dato['direccion'] ?></div>
+                                <div class="telPiz"><?php echo $dato['telefonoPizzeria'] ?></div>
+                                <a href="<?php echo $dato['urlFacebook'] ?>" target="_blank"><img src="images/logoFacebook.png" alt=""></a>
+                                <a href="<?php echo $dato['urlMaps'] ?>" target="_blank"><img src="images/logoMaps.png" alt=""></a>
+                            </div>
+                        <?php endforeach ?>
                     </div>
                 </div>
             </div>
         </div>
-        <a href="index.php" style="text-decoration-line: none;">
-            <h1 class="boton">
-                Regresar
-            </h1>
-        </a>
+        <div class="d-flex justify-content-center">
+            <div type="button" class="btn btn-danger">Regresar</div>
+        </div>
     </section>
 
 
